d3895fb70296a8394ea0b2db2e778e74ec76a42f
dgwp-projects <zayneabraham1@gmail.com>
2017-03-22 23:18:10 -0400

    done

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 function showRepositories(event, data) {
-  var repos = JSON.parse(this.responseText)
-  console.log(repos)
-  const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
+  const repos = JSON.parse(this.responseText)
+  const repoList = `<ul>${repos.map(r => '<li><a href="' + r.html_url + '">' + r.name + '</a></li>').join('')}</ul>`
   document.getElementById("repositories").innerHTML = repoList
 }
 
@@ -25,3 +24,4 @@ function showCommits() {
   const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.login + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`
   document.getElementById("commits").innerHTML = commitsList
 }
+
